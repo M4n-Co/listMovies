0eeb76ab265e617c31f301b9a14de15569f490e0
M4npr <M4nprz.H@hotmail.com>
2023-02-09 14:51:46 -0600

    Se agrego el logging interceptor

--- app/src/main/java/com/example/listmovies/interface/RetrofitService.kt
+++ app/src/main/java/com/example/listmovies/interface/RetrofitService.kt
@@ -1,7 +1,8 @@
 package com.example.listmovies.`interface`
 
-
 import com.example.listmovies.DCListMovies.MovieItem
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,10 +14,17 @@ interface RetrofitService {
 
     companion object{
         var retrofitService: RetrofitService? = null
+
         fun getInstance() : RetrofitService{
+
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client  = OkHttpClient.Builder().addInterceptor(logging).build()
+
             if (retrofitService == null){
                 val retrofit = Retrofit.Builder()
                     .baseUrl("https://howtodoandroid.com/")
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
                 retrofitService = retrofit.create(RetrofitService::class.java)
